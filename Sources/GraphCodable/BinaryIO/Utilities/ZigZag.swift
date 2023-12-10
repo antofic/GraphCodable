c8f3c1853629807f74dce9391c8d31265d1780ea
antofic <ing.ficarra@gmail.com>
2023-12-10 10:59:56 +0100

    Zigzag modify

--- Sources/GraphCodable/BinaryIO/Utilities/ZigZag.swift
+++ Sources/GraphCodable/BinaryIO/Utilities/ZigZag.swift
@@ -6,7 +6,34 @@
 
 import Foundation
 
-enum ZigZag {
+protocol ZigZag : FixedWidthInteger {
+	associatedtype Counterpart : ZigZag
+	init( bitPattern: Self.Counterpart )
+}
+
+extension UInt16	: ZigZag	{ typealias  Counterpart = Int16 }
+extension UInt32	: ZigZag	{ typealias  Counterpart = Int32 }
+extension UInt64	: ZigZag	{ typealias  Counterpart = Int64 }
+
+extension Int16		: ZigZag	{ typealias  Counterpart = UInt16 }
+extension Int32		: ZigZag	{ typealias  Counterpart = UInt32 }
+extension Int64		: ZigZag	{ typealias  Counterpart = UInt64 }
+
+extension ZigZag where Self: SignedInteger, Self == Self.Counterpart.Counterpart {
+	/// Transform signed integers to unsigned with zigzag algorithm so that packing is possible.
+	var zigZagEncoded : Counterpart {
+		(Counterpart(bitPattern: self) &+ Counterpart(bitPattern: self)) ^ Counterpart(bitPattern:( self &>> (self.bitWidth - 1) ))
+	}
+}
+
+extension ZigZag where Self: UnsignedInteger, Self == Self.Counterpart.Counterpart {
+	/// Transform unsigned integers to signed so that unpacking is possible.
+	var zigZagDecoded : Counterpart {
+		Counterpart( bitPattern: (self &>> 1) ^ (0 &- (self & 1)) )
+	}
+}
+/*
+enum ZigZagOLD {
 	/// Transform signed integers to unsigned so that packing is possible.
 	static func encode(  _ v:Int16 ) -> UInt16 {
 		(UInt16(bitPattern: v) &+ UInt16(bitPattern: v)) ^ UInt16(bitPattern:( v &>> (v.bitWidth - 1) ))
@@ -33,3 +60,4 @@ enum ZigZag {
 		Int64( bitPattern: (v &>> 1) ^ (0 &- (v & 1)) )
 	}
 }
+*/
